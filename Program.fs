bf0812fed77067347e3dc68719d1c08c40699fe8
J2ghz <j2.00ghz@gmail.com>
2019-12-26 16:36:23 +0100

    example

--- Program.fs
+++ Program.fs
@@ -4,10 +4,20 @@ open System
 open JsonModule
 open FSharp.Data
 open XPlot.GoogleCharts
+open Deedle
 
 [<EntryPoint>]
 let main argv =
-    printArr
+    let names = JsonModule.passengers
+                |> Array.map (fun passenger -> passenger.Fields.Name)
+    printfn "%A" names           
+
+    let avgAgeBySex =
+        passengers
+        |> Array.groupBy (fun p -> p.Fields.Sex)
+        |> Array.map (fun (s,ps) -> s,ps |> Array.averageBy (fun p -> p.Fields.Age |> Option.defaultValue 0m))
+    printfn "%A" avgAgeBySex
+
     // let res = total
     // printfn "%A" total
 
