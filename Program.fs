2063f52e9e4e66814742b40f027e2660d3224d0a
J2ghz <j2.00ghz@gmail.com>
2019-12-29 16:06:12 +0100

    airquality

--- Program.fs
+++ Program.fs
@@ -3,16 +3,26 @@
 open System
 open XPlot.GoogleCharts
 
+type AirQual = FSharp.Data.XmlProvider<"./data/pollution.xml">
+
 [<EntryPoint>]
 let main argv =
-
-    Json.showTitanicAmountChart 700 700
-    Json.showTitanicAgeChart 700 700
-    Json.missileSuccessRate 900 900
-    Json.showCommodityValueLineChart (["Copper"; "Lead"; "Aluminum"; "Nickel"; "Tin"] |> Array.ofList) 900
-
-    CSV.ageToFare
-    |> Chart.WithHeight 1000    
+    AirQual.GetSample().Rows
+    |> Array.groupBy (fun d -> d.Country)
+    |> Array.map (fun (c,d) -> c, d |> Array.averageBy (fun a -> a.Value |> double))
+    |> Array.choose (fun (c,d) -> match c with | Some cc -> Some (cc,d) | None -> None )
+    |> Chart.Geo
     |> Chart.Show
+    
+    // Json.showTitanicAmountChart 700 700
+    // Json.showTitanicAgeChart 700 700
+    // Json.missileSuccessRate 900 900
+    // Json.showCommodityValueLineChart (["Copper"; "Lead"; "Aluminum"; "Nickel"; "Tin"] |> Array.ofList) 900
+
+    // CSV.ageToFare
+    // |> Chart.WithHeight 1000    
+    // |> Chart.Show
+
+    
 
     0 // return an integer exit code
